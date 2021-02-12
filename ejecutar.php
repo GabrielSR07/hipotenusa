7d6923c34e9fc8cb29bd8d84499d8b0e083143f9
GabrielSR07 <59975040+GabrielSR07@users.noreply.github.com>
2021-02-12 10:13:32 -0400

    Add files via upload

---
+++ ejecutar.php
@@ -0,0 +1,15 @@
+<?php 
+
+$c1 = 3;
+$c2 = 4;
+$hip = 0;
+
+
+$hip = sqrt(pow(3, 2)+pow(4, 2));
+
+echo 'Calculo del area del triangulo', '<br/>';
+echo 'Lado 1 del triangulo:'.$c1, '<br/>';
+echo 'Lado 2 del triangulo:'.$c2, '<br/>';
+echo 'La hipotenusa de este triangulo es:'.$hip, '<br/>';
+
+ ?>
